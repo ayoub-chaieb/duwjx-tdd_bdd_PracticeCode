bec2f24254a9a0b91b163bcf6d60644d9df673c2
Ayoub Chaieb <ayoubchaieb75@gmail.com>
2025-10-03 20:48:18 +0200

    Implement step to load and refresh pets data
    Refreshes the pets database by deleting existing pets and loading new ones from the context table.

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -10,4 +10,25 @@ import requests
 from behave import given
 
 # Load data here
-
+@given("the following pets")
+def step_impl(context):
+    """Refresh all Pets in the database"""
+    # List all pets and delete them one by one
+    response = requests.get(f"{context.base_url}/pets")
+    assert 200 == response.status_code
+    for pet in response.json():
+        response = requests.delete(f"{context.base_url}/pets/{pet['id']}")
+        assert 204 == response.status_code
+    
+    # load the database with new pets
+    for row in context.table:
+        payload= {}
+        
+        payload["name"] = row["name"]
+        payload["category"] = row["category"]
+        payload["available"] = row["available"] in ["True", "true", 1, ]
+        payload["gender"] = row["gender"]
+        payload["birthday"] = row["birthday"]
+        
+        response = requests.post(f"{context.base_url}/pets", json=payload)
+        assert 201 == response.status_code
